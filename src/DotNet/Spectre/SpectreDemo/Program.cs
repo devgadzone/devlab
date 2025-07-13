3063ca0d776976de954afb20449356615eb5f8e1
Dariusz Gał <dgal@devgadzone.pl>
2025-07-13 11:45:12 +0200

    feat: Add Spectre demo project init

---
+++ src/DotNet/Spectre/SpectreDemo/Program.cs
@@ -0,0 +1,8 @@
+﻿using Spectre.Console;
+
+AnsiConsole.MarkupLine("[red bold]Hello World from Spectre[/]");
+AnsiConsole.MarkupLine("Hello World from Spectre");
+AnsiConsole.MarkupLine("[slowblink]Hello World from Spectre[/]");
+
+Console.ReadLine();
+AnsiConsole.Clear();
