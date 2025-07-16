01fb5dfd9231cab534cbc8d4cdd36a2df87b83e5
Dariusz Gał <dgal@devgadzone.pl>
2025-07-16 18:14:52 +0200

    Spectre: Requesting Data from User

--- src/DotNet/Spectre/SpectreDemo/Program.cs
+++ src/DotNet/Spectre/SpectreDemo/Program.cs
@@ -13,6 +13,7 @@ AnsiConsole.MarkupLine("[slowblink]Hello World from Spectre[/]");
 
 #region Colors and Styles
 
+/*
 AnsiConsole.MarkupLine("[red]This is the inline markup.[/]");
 AnsiConsole.MarkupLine("[red on white]This is the inline markup.[/]");
 AnsiConsole.MarkupLine("[red on white bold]This is the inline markup.[/]");
@@ -25,6 +26,34 @@ Style danger = new(
 AnsiConsole.Write("demo text ");
 AnsiConsole.Write(new Markup("Danger Text from style", danger));
 AnsiConsole.WriteLine(" and more..");
+*/
+
+#endregion
+
+
+#region Requesting Data from the user
+
+int age = AnsiConsole.Ask<int>("Waht is your age?");
+bool isHappy = AnsiConsole.Ask<bool>("Are you happy?");
+string happyText = AnsiConsole.Prompt(
+    new TextPrompt<string>("Are you happy?")
+        .AddChoice("yes")
+        .AddChoice("no")
+        .DefaultValue("yes")
+);
+
+int age2 = AnsiConsole.Prompt(
+    new TextPrompt<int>("What is your age?")
+        .Validate((x) => x switch
+        {
+            < 0 => ValidationResult.Error("You are not born yet :)"),
+            > 150 => ValidationResult.Error("You are too old :)"),
+            _ => ValidationResult.Success()
+        })
+);
+
+
+AnsiConsole.MarkupLine($"Happy: {isHappy}\nAge: {age}\nHappy: {happyText}\nAge2: {age2}");
 
 #endregion
 
