d9913414fcdd251742cd50b8c09c5467db1b9ac7
Dariusz Gał <dgal@devgadzone.pl>
2025-07-16 18:00:20 +0200

    Spectre: Colors and Styles

--- src/DotNet/Spectre/SpectreDemo/Program.cs
+++ src/DotNet/Spectre/SpectreDemo/Program.cs
@@ -1,8 +1,32 @@
 ﻿using Spectre.Console;
 
+#region Initial demo
+
+/*
 AnsiConsole.MarkupLine("[red bold]Hello World from Spectre[/]");
 AnsiConsole.MarkupLine("Hello World from Spectre");
 AnsiConsole.MarkupLine("[slowblink]Hello World from Spectre[/]");
+*/
+
+#endregion
+
+
+#region Colors and Styles
+
+AnsiConsole.MarkupLine("[red]This is the inline markup.[/]");
+AnsiConsole.MarkupLine("[red on white]This is the inline markup.[/]");
+AnsiConsole.MarkupLine("[red on white bold]This is the inline markup.[/]");
+
+Style danger = new(
+    foreground: Color.White,
+    background: Color.DarkOrange,
+    decoration: Decoration.Bold | Decoration.Italic
+);
+AnsiConsole.Write("demo text ");
+AnsiConsole.Write(new Markup("Danger Text from style", danger));
+AnsiConsole.WriteLine(" and more..");
+
+#endregion
 
 Console.ReadLine();
 AnsiConsole.Clear();
