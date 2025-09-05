88e9e764bc073fb2e54a5725c77f22569403f1f7
Dariusz Gał <dgal@devgadzone.pl>
2025-09-05 20:23:35 +0200

    feat: add ISqlDataAccess interface to define async data access methods

---
+++ src/DotNet/SimpleTodoList/src/ConsoleSimpleTodoList/Persistances/ISqlDataAccess.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsoleSimpleTodoList.Persistances;
+
+public interface ISqlDataAccess
+{
+    Task<List<T>> LoadData<T, U>(string command, U parameters, CommandType commandType, string connectionSTring);
+    Task SaveData<T>(string command, T parameters, CommandType commandType, string connectionSTring);
+}
