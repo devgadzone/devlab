c0a2f551f076348f78bbc06cd0d2f3dfd97d4d8a
Dariusz Gał <dgal@devgadzone.pl>
2025-09-05 20:49:04 +0200

    feat: add ITodoRepository interface to define CRUD operations for Todo management

---
+++ src/DotNet/SimpleTodoList/src/ConsoleSimpleTodoList/Repositories/ITodoRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleSimpleTodoList.Repositories;
+
+public interface ITodoRepository<T, TKey>
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(TKey id);
+    Task<int> AddAsync(T model);
+    Task<int> UpdateAsync(T model);
+    Task<int> DeleteAsync(int id);
+}
