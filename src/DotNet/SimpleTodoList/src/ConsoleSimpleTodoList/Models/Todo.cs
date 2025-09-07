ca2dd2eee0345562cc34600fc546825073901b36
Dariusz Gał <dgal@devgadzone.pl>
2025-09-07 10:51:00 +0200

    refactor: make UpdatedAt optional in Todo model

--- src/DotNet/SimpleTodoList/src/ConsoleSimpleTodoList/Models/Todo.cs
+++ src/DotNet/SimpleTodoList/src/ConsoleSimpleTodoList/Models/Todo.cs
@@ -6,5 +6,5 @@ public class Todo
     public required string Description { get; set; }
     public bool IsDone { get; set; }
     public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
 }
