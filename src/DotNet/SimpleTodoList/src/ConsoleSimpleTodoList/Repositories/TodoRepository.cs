9c67be2a76803f2d6198ea8c30e48aaaf370861b
Dariusz Gał <dgal@devgadzone.pl>
2025-09-07 10:19:49 +0200

    feat: add TodoRepository with method stubs for CRUD operations

---
+++ src/DotNet/SimpleTodoList/src/ConsoleSimpleTodoList/Repositories/TodoRepository.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleSimpleTodoList.Repositories;
+
+public class TodoRepository<T, TKey> : ITodoRepository<T, TKey>
+{
+    public Task<IEnumerable<T>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T?> GetByIdAsync(TKey id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> AddAsync(T model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> UpdateAsync(T model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
