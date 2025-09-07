be048df7141a9319892573edd868bc7427e07e3a
Dariusz Gał <dgal@devgadzone.pl>
2025-09-07 10:18:05 +0200

    feat: add SqlDataAccess implementation with method stubs for data loading and saving

---
+++ src/DotNet/SimpleTodoList/src/ConsoleSimpleTodoList/Persistances/SqlDataAccess.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleSimpleTodoList.Persistances;
+
+public class SqlDataAccess : ISqlDataAccess
+{
+    public Task<IEnumerable<T>> LoadData<T, U>(string command, U parameters, CommandType commandType)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SaveData<T>(string command, T parameters, CommandType commandType)
+    {
+        throw new NotImplementedException();
+    }
+}
