87e7b7b997af1f39dfb6a7a2f3e18aa7a296f7ee
Dariusz Gał <dgal@devgadzone.pl>
2025-09-07 17:23:29 +0200

    chore: update ISqlDataAccess to use generic TKey for SaveDataAsync return type

--- src/DotNet/SimpleTodoList/src/ConsoleSimpleTodoList/Persistances/ISqlDataAccess.cs
+++ src/DotNet/SimpleTodoList/src/ConsoleSimpleTodoList/Persistances/ISqlDataAccess.cs
@@ -1,7 +1,7 @@
 ﻿namespace ConsoleSimpleTodoList.Persistances;
 
-public interface ISqlDataAccess
+public interface ISqlDataAccess<TKey>
 {
     Task<IEnumerable<T>> LoadDataAsync<T, U>(string command, U parameters, CommandType commandType);
-    Task SaveDataAsync<T>(string command, T parameters, CommandType commandType);
+    Task<TKey> SaveDataAsync<T>(string command, T parameters, CommandType commandType);
 }
