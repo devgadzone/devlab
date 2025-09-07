3bfb1f06aa469d3ae8db0ea93cb1fea064c41301
Dariusz Gał <dgal@devgadzone.pl>
2025-09-07 10:30:48 +0200

    refactor: make CreatedAt optional in Todo model

--- src/DotNet/SimpleTodoList/src/ConsoleSimpleTodoList/Models/Todo.cs
+++ src/DotNet/SimpleTodoList/src/ConsoleSimpleTodoList/Models/Todo.cs
@@ -5,6 +5,6 @@ public class Todo
     public int Id { get; set; }
     public required string Description { get; set; }
     public bool IsDone { get; set; }
-    public required DateTime CreatedAt { get; set; }
+    public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 }
