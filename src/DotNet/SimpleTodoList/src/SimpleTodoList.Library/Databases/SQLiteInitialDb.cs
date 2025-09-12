7c19e812efb57e44350ef5242e736e1c25d59380
Dariusz Gał <dgal@devgadzone.pl>
2025-09-12 15:14:08 +0200

    chore: add CHECK constraint for IsDone column in SQLiteInitialDb setup

--- src/DotNet/SimpleTodoList/src/SimpleTodoList.Library/Databases/SQLiteInitialDb.cs
+++ src/DotNet/SimpleTodoList/src/SimpleTodoList.Library/Databases/SQLiteInitialDb.cs
@@ -23,7 +23,7 @@ public class SQLiteInitialDb<TKey> : ISqlInitialDb<TKey>
                                   CREATE TABLE IF NOT EXISTS main.Todos (
                                       Id INTEGER PRIMARY KEY AUTOINCREMENT,
                                       Description TEXT NOT NULL,    
-                                      IsDone INTEGER NOT NULL DEFAULT 0,
+                                      IsDone INTEGER NOT NULL DEFAULT 0 CHECK(IsDone IN (0,1)),
                                       CreatedAt TEXT NOT NULL DEFAULT (datetime('now')),
                                       UpdatedAt TEX
                                   );
